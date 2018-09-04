4c4d1efb587672e9da54eb71c28c83d597f7e60f
serpantin99 <fDfXfniXfni123>
2018-09-04 16:06:03 +0200

    first Request

---
+++ app/src/main/java/com/wallstreetsim/background/RequestMaker.java
@@ -0,0 +1,40 @@
+package com.wallstreetsim.background;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class RequestMaker extends AsyncTask<String, Void, Response> {
+    protected Response doInBackground(String... args) {
+        String url = args[0];
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("Cache-Control", "no-cache")
+                .build();
+
+        Response response = null;
+        try {
+            response = client.newCall(request).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+    protected void onProgressUpdate(Integer... progress) {
+
+    }
+
+    protected void onPostExecute(Response result) {
+        Log.i("Response: ", result.toString());
+    }
+}
